425f8ca89fcbfa403e27f772ce06b70b5c5cfaf6
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-04-20 12:49:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#Include <stdio.h
+#include <stdio.h
 /**
  * main - A program that prints puts function
  * Return: 0
  */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
