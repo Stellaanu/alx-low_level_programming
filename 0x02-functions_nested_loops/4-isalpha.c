be7a373db7b93840f6cbe0cebd286f9e5429c4fb
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-04-26 02:53:54 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,17 @@
 /**
  * _isalpha - shows 1 if the input is a
  * letter. Another cases, shows 0
+ *
  * @c: The character in ASCII code
  * Return: 1 for letters. 0 for the rest.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
