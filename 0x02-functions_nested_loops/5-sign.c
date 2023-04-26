1985ae327dd037a40a3668616cfba1e5b33b6f30
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-04-26 05:44:42 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  *Return: 1 is grester than zero. 0 is zero.
  *-1 is less than zero.
 */
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
@@ -15,7 +15,7 @@ int print_sign(int n);
 		return (1);
 	}
 
-		else if (n < 0);
+		else if (n < 0)
 	{
 		_putchar (45);
 		return (-1);
