53a03af6b83b72de004a606567b8a0bf5009aa9f
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-05-02 03:08:06 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes apointer to an int a  s parameter
+ * an update that values it points to to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n  98;
+}
+
