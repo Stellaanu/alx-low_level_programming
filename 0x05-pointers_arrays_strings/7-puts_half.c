24a86e36a95b62df32fa69fea58c3a76fd74a797
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-05-02 12:08:15 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,27 @@
 #include "main.h"
 /**
- * _strlen - returns the length of the string
- * @s: string
- * Return: length
+ * puts_half - print the second half of the string
+ * @s: string to be printed
+ *
+ * Return: nothing on success
  */
-vi 
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (*str)
+	{
+		count++;
+		str++;
+	}
+
+	for (i = 0; i < count; i++)
+		str--;
+
+	i = (count % 2 == 0) ? count / 2 : (count + 1) / 2;
+
+	for (; i, count; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
