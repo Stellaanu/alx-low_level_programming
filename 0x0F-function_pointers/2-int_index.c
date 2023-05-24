d91a1dcedfccc6f11dd7ebf98e294b59f33289f4
Anuoluwapo Akinde <stellaanuoluwapo8@gmail.com>
2023-05-24 16:39:54 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,21 @@
  * @size: size of the array
  * @cmp: pointer to the function to be used to compare values
  * Return: if no element matches or size <=0 - -1
- * Otherwise - the index of the first element for which the cmp function does not return 0
+ * 	Otherwise - the index of the first element for which the
+ * 		the cmp function does not return 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (array == NULL|| cmp ==  NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
+
 	for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index]) != 0)
 			return (index);
 	}
+
 	return (-1);
 }
